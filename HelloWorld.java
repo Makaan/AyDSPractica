14afc033f29783755f0bc3df6aac02aa009e8f2b
gonzaloGiampietri <giampietri.gonzalo@gmail.com>
2019-03-13 09:23:43 -0300

    Para Henry <3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public void static main(String[] args) {
-        System.out.println("Hola manola");
-	   System.out.println("Rick y Mory");
+        System.out.println("Tierne");
+	   System.out.println("Henry y Dylen");
     }
 }
